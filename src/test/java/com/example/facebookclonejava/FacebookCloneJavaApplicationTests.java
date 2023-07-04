3dd71a909d4240d2c9b3ce9707c3ae7cf7c11ff1
Yosef <yosef.saaid02@eng-st.cu.edu.eg>
2023-07-04 20:25:38 +0300

    first commit

---
+++ src/test/java/com/example/facebookclonejava/FacebookCloneJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.facebookclonejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookCloneJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
